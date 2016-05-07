bb855a269a3db31c835c1bf155128b4ead0a9ecc
Felipe Marin <lfelipe.marin@gmail.com>
2016-05-07 11:33:45 -0500

    Proyecto vacio lab 4 weather

---
+++ L4GX-Weather/app/src/main/java/co/edu/udea/compumovil/gr1/weather/l4gx_weather/MainActivity.java
@@ -0,0 +1,15 @@
+package co.edu.udea.compumovil.gr1.weather.l4gx_weather;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static String API_KEY = "8839b7df4f87730b86c09729e2c9c67f";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
